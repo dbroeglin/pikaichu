1053fa29eb8b9a31385f8539de64136ced22b090
Dominique Broeglin <dominique.broeglin@gmail.com>
2021-12-12 17:33:51 +0100

    Made SMTP domain configurable by ENV['SENDGRID_DOMAIN']

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => ENV['SENDGRID_DOMAIN'],
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
