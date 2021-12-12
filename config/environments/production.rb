af22d965725d3ec9c50606314faa1f064f591c73
Dominique Broeglin <dominique.broeglin@gmail.com>
2021-12-12 17:14:03 +0100

    Chaning domain for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => ENV['DEFAULT_URL'],
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
