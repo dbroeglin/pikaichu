8aa265a735cc2b79f4c756d12eb822079adb40b0
Dominique Broeglin <dominique.broeglin@gmail.com>
2021-12-16 19:55:27 +0100

    Nicer form placement and size, added code prettyfier

--- config/initializers/simple_form_bulma.rb
+++ config/initializers/simple_form_bulma.rb
@@ -77,19 +77,19 @@ SimpleForm.setup do |config|
     #end
   end
 
-    config.wrappers :vertical_text, tag: "div", class: "field" do |b|
-        b.use :html5
-        b.use :placeholder
-        b.optional :maxlength
-        b.optional :minlength
-        b.optional :pattern
-        b.optional :min_max
-        b.optional :readonly
-        b.use :label, class: "label"
-        b.use :input, class: "textarea", wrap_with: { tag: "div", class: "control" }, error_class: "is-danger", valid_class: "is-success"
-        b.use :full_error, wrap_with: { tag: "div", class: "help is-danger" }
-        #b.use :hint, wrap_with: { tag: "small", class: "form-text text-muted" }
-    end
+  config.wrappers :vertical_text, tag: "div", class: "field" do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label, class: "label"
+    b.use :input, class: "textarea", wrap_with: { tag: "div", class: "control" }, error_class: "is-danger", valid_class: "is-success"
+    b.use :full_error, wrap_with: { tag: "div", class: "help is-danger" }
+    #b.use :hint, wrap_with: { tag: "small", class: "form-text text-muted" }
+  end
 
 
 
