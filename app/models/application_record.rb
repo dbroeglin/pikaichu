2ad23530d7cb1930eb87ac59c0dddb9dcb2c0d70
Dominique Broeglin <dominique.broeglin@gmail.com>
2021-12-26 13:47:50 +0100

    Added meta method for human_[enum_name] method generation

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,19 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   def self.human_enum_value(enum_name, enum_value)
-    I18n.t("activerecord.enums.#{model_name.i18n_key}.#{enum_name}.#{enum_value}",
-           default: human_attribute_name(enum_value))
+    if enum_value.nil?
+      nil
+    else
+      I18n.t("activerecord.enums.#{model_name.i18n_key}.#{enum_name}.#{enum_value}",
+            default: enum_value.humanize)
+    end
+  end
+
+  class << self
+    def human_enum(enum_name)
+      self.send(:define_method, "human_#{enum_name}") do
+        ApplicationRecord.human_enum_value(enum_name, self.send(enum_name))
+      end
+    end
   end
 end
