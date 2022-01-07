ed9c456667385ffdf162d70b0201dd25bb186cd8
Dominique Broeglin <dominique.broeglin@gmail.com>
2022-01-07 17:21:48 +0100

    Started I18n and L10n for FR (default locale: FR)

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,7 +2,9 @@ FactoryBot.define do
   factory :user do
     firstname { Faker::Name.first_name  }
     lastname { Faker::Name.last_name }
-    email { "#{firstname}.#{lastname}@example.org".downcase }
+    email {
+       "#{ ActiveSupport::Inflector.parameterize "#{firstname}.#{lastname}", separator: "."}@example.org"
+    }
     password { "password" }
     confirmed_at { DateTime.now }
   end
