f392de73ec7d3fa2b9bc7f066cfddeb3514d0c4b
Dominique Broeglin <dominique.broeglin@microsoft.com>
2024-11-23 16:57:30 +0100

    WIP polishing Scoreboard JSON format to fit Pikaled pydantic

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -7,54 +7,56 @@ class ScoreboardController < ApplicationController
     @scoreboard = Scoreboard.find_by!(api_key: params[:api_key])
 
     @tachi = @scoreboard.participating_dojo.current_tachi
+    @taikai = @scoreboard.participating_dojo.taikai
 
     # answer different formats
     respond_to do |format|
       format.html do
-        @taikai = @scoreboard.participating_dojo.taikai
       end
       format.json do
-        taikai = @scoreboard.participating_dojo.taikai
-
-        render json: {
-          index: @tachi.index,
-          round: @tachi.round,
+        data = {
           taikai: {
-            name: taikai.name,
-            shortname: taikai.shortname,
-            form: taikai.form,
-            num_targets: taikai.num_targets,
-            num_arrows: taikai.num_arrows,
-            num_rounds: taikai.num_rounds,
-            scoring: taikai.scoring,
-            total_num_arrows: taikai.total_num_arrows
+            name: @taikai.name,
+            shortname: @taikai.shortname,
+            form: @taikai.form,
+            num_targets: @taikai.num_targets,
+            num_arrows: @taikai.num_arrows,
+            num_rounds: @taikai.num_rounds,
+            scoring: @taikai.scoring,
+            total_num_arrows: @taikai.total_num_arrows
           },
-          participating_dojo: {
-            name: @scoreboard.participating_dojo.display_name,
-          },
-          results: [
-            @tachi.participants.map do |participant|
-              score = participant.scores.first
-              {
-                name: participant.display_name,
-                index: participant.index,
-                score: {
-                  results: score.results.round(@tachi.round).map do |result|
-                    {
-                      status: result.status,
-                      value: result.value,
-                      final: result.final
-                    }
-                  end
-                },
-                team: {
-                  shortname: participant.team.shortname,
-                  index: participant.team.index
-                },
-              }
-            end
-          ]
         }
+        if @tachi            
+          data.merge!(
+            tachi: {
+              index: @tachi.index,
+              round: @tachi.round,
+              participating_dojo: {
+                name: @scoreboard.participating_dojo.display_name,
+              },
+              participants: @tachi.participants.map do |participant|
+                score = participant.scores.first
+                {
+                  name: participant.display_name,
+                  index: participant.index,
+                  score: {
+                    results: score.results.round(@tachi.round).map do |result|
+                      {
+                        status: result.status,
+                        value: result.value,
+                        final: result.final
+                      }
+                    end
+                  },
+                  team: {
+                    shortname: participant.team.shortname,
+                    index: participant.team.index
+                  },
+                }
+              end
+            })
+        end
+        render json: data 
       end
     end
   end
